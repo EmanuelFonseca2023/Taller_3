b3ad6c23959bdb25fb954facba099472876807fb
EmanuelFonseca2023 <emanueldavidfonsecamontagut@gmail.com>
2025-03-20 19:57:00 -0500

    Final

--- Taller_4.py
+++ Taller_4.py
@@ -1,23 +1,24 @@
 import sqlite3
 con = sqlite3.connect("DataBase.db")
 cur = con.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, email TEXT)""")
+cur.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,telefono INTEGER,email TEXT,ciudad TEXT,direccion TEXT)""")
 con.commit()
-def create_users(name,email):
-    cur.execute("INSERT INTO users(name,email) VALUES (?,?)",(name,email))
+def create_users(name,telefono,email,ciudad,direccion):
+    cur.execute("INSERT INTO users(name,telefono,email,ciudad,direccion) VALUES (?,?,?,?,?)",(name,telefono,email,ciudad,direccion))
     con.commit()
 def leer_users():
     cur.execute("SELECT * FROM users")
     return cur.fetchall()
-def actualizar_users(id,name,email):
-    cur.execute("UPDATE users SET name =?, email=? WHERE id =?",(name,email,id))
+def actualizar_users(id,name,telefono,email,ciudad,direccion):
+    cur.execute("UPDATE users SET name =?,telefono=? ,email=?,ciudad=?,direccion=? WHERE id =?",(name,telefono,email,ciudad,direccion,id))
     con.commit()
 def borrar_users(id):
     cur.execute("DELETE FROM users WHERE id =?",(id,))
     con.commit()
     
 while True:
-    x=int(input("""Que te gustaria hacer en la base de datos.
+    x=int(input("""
+Que te gustaria hacer en la base de datos.
     0.Cerrar el programa
     1.Crear un usuario
     2.Mostrar Base de datos
@@ -29,15 +30,15 @@ while True:
         print("Muchas gracias por usar nuestra aplicacion")
         break
     elif x==1:
-        datos=list(input("Para añadir un usuario tienes que escribir el nombre y email separado por una , ").split(","))
-        create_users(datos[0].strip(), datos[1].strip())
+        datos=list(input("Para añadir un usuario tienes que escribir el nombre, telefono, email,cidad,direccion separado por una , ").split(","))
+        create_users(datos[0].strip(), int(datos[1]),datos[2].strip(),datos[3].strip(),datos[4].strip())
         continue
     elif x==2:
         print(leer_users())
         continue
     elif x==3:
-        datos=list(input("Dame el id nombre y email todos separados por , ").split(","))
-        actualizar_users(int(datos[0]), datos[1].strip(), datos[2].strip())
+        datos=list(input("Dame el id nombre,telefono, email, ciudad,direccion todos separados por , ").split(","))
+        actualizar_users(int(datos[0]),datos[1].strip(),int(datos[2]),datos[3].strip(),datos[4].strip(),datos[5].strip())
         continue
     elif x==4:
         datos=int(input("Que usuario quiere borrar? "))
